c39cc41c57a04d558d154a25a2f50ba5bb0b5e29
jooyoung <mirkduddl@naver.com>
2022-05-09 21:41:23 +0900

    component & props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import './Box.css';
+import Box from './Box'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>react study start!</h1>
-    </div>
-  );
+function App(){
+    return (
+		<>
+			<Box name="리액트" num="1"/>
+			<Box name="너무"  num="2"/>
+			<Box name="어려워요"  num="3"/>
+			<Box name="ㅠㅠ"  num="4"/>
+		</>
+	)
 }
 
 export default App;
