423236e5dd2fd7d84701b2ecd8024b0ba37e4a9c
jooyoung <mirkduddl@naver.com>
2022-05-09 22:08:45 +0900

    Component & Props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Box from './Box';
 
 function App() {
   return (
     <div className="App">
-      <h1>react study start!</h1>
+      <Box name="리액트" num="1"/>
+      <Box name="너무 어려워요..." num="2"/>
     </div>
   );
 }
